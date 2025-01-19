bf1a4e2f816a6e7ada41b5c74abd5193db4532cc
Kabindra <kabindra@Kabindras-MacBook-Pro.local>
2025-01-19 23:38:46 +0545

    hexagon view created in canvas and added in account page, blob view creation

--- Course/HexagonView.swift
+++ Course/HexagonView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 struct HexagonView: View {
     var body: some View {
         Canvas { context, size in
-            context.draw(Text("DesignCode"), at: CGPoint(x: 50, y: 10))
+//            context.draw(Text("DesignCode").font(.largeTitle), at: CGPoint(x: 50, y: 10))
+//            context.fill(Path(ellipseIn: CGRect(x: 0, y: 0, width: 100, height: 100)), with: .color(.red))
+            context.draw(Image("Blob 1"), in: CGRect(x: 0, y: 0, width: 200, height: 200))
+            context.draw(Image(systemName: "hexagon.fill"), in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            
         }
+        .frame(width: 200, height: 210)
+        .foregroundStyle(.linearGradient(colors: [.pink, .blue], startPoint: .topLeading, endPoint: .bottomTrailing))
     }
 }
 
